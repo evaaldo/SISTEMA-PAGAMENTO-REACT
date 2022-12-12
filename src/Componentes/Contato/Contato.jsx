a06de87a3dd9f31ef930da5b885365e83e79bb4a
evaaldo <evaaldormj@gmail.com>
2022-12-12 11:08:13 -0300

    feat: Adicionando título a aba de contato

--- src/Componentes/Contato/Contato.jsx
+++ src/Componentes/Contato/Contato.jsx
@@ -42,40 +42,44 @@ const Contato = () => {
 
     return(
         <div className='contato' id='contato'>
-            <div className='contato-dados'>
-                <h2 className='contato-dados__subtitulo'>Barbearia</h2>
-                <h1 className='contato-dados__titulo'>Contato</h1>
-                <div className='linha'></div>
-                <div className='contato-dados__itens'>
-                    <div className='contato-dados__item'>
-                        <HiLocationMarker/><p className='item-contato'>Rua ou avenida, endereço completo.</p>
+            <h1 className='contato__titulo'>Como nos contatar?</h1>
+            <div className='contato-dados-talk'>
+                <div className='contato-dados'>
+                    <h2 className='contato-dados__subtitulo'>Barbearia</h2>
+                    <h1 className='contato-dados__titulo'>Contato</h1>
+                    <div className='linha'></div>
+                    <div className='contato-dados__itens'>
+                        <div className='contato-dados__item'>
+                            <HiLocationMarker/><p className='item-contato'>Rua ou avenida, endereço completo.</p>
+                        </div>
+                        <div className='contato-dados__item'>
+                            <BsTelephoneFill/><p className='item-contato'>(85) 9.9612.8272</p>
+                        </div>
+                        <div className='contato-dados__item'>
+                            <FiMail/><p className='item-contato'>nome@escritorio.com.br</p>
+                        </div>
                     </div>
-                    <div className='contato-dados__item'>
-                        <BsTelephoneFill/><p className='item-contato'>(85) 9.9612.8272</p>
-                    </div>
-                    <div className='contato-dados__item'>
-                        <FiMail/><p className='item-contato'>nome@escritorio.com.br</p>
+                    <div className='linha'></div>
+                    <h1 className='contato-dados__titulo'>Redes sociais</h1>
+                    <div className='contato-dados__redes-sociais'>
+                        <a href='#'><FaFacebookSquare color='white' size={32}/></a>
+                        <a href='#'><FaLinkedin color='white' size={32}/></a>
+                        <a href='#'><FaInstagramSquare color='white' size={32}/></a>
                     </div>
                 </div>
-                <div className='linha'></div>
-                <h1 className='contato-dados__titulo'>Redes sociais</h1>
-                <div className='contato-dados__redes-sociais'>
-                    <a href='#'><FaFacebookSquare color='white' size={32}/></a>
-                    <a href='#'><FaLinkedin color='white' size={32}/></a>
-                    <a href='#'><FaInstagramSquare color='white' size={32}/></a>
-                </div>
+                <div className='contato-talk'>
+                    <h2 className='contato-talk__subtitulo'>Sem compromisso</h2>
+                    <h1 className='contato-talk__titulo'>Fale conosco</h1>
+                    <form className='contato-talk__formulario' onSubmit={sendEmail}>  
+                        <input required className='contato-talk__nome' type='text' placeholder='Nome completo' onChange={(e) => setNome(e.target.value)} value={nome}></input>
+                        <input required className='contato-talk__numero' type='text' placeholder='Número' onChange={(e) => setNumero(e.target.value)} value={numero}></input>
+                        <input required className='contato-talk__email' type='text' placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} value={email}></input>
+                        <input required className='contato-talk__mensagem' type='text' placeholder='Mensagem' onChange={(e) => setMensagem(e.target.value)} value={mensagem}></input>
+                        <input required value='Enviar' type='submit' className='contato-talk__botao'></input>
+                    </form>
+                </div>    
             </div>
-            <div className='contato-talk'>
-                <h2 className='contato-talk__subtitulo'>Sem compromisso</h2>
-                <h1 className='contato-talk__titulo'>Fale conosco</h1>
-                <form className='contato-talk__formulario' onSubmit={sendEmail}>  
-                    <input required className='contato-talk__nome' type='text' placeholder='Nome completo' onChange={(e) => setNome(e.target.value)} value={nome}></input>
-                    <input required className='contato-talk__numero' type='text' placeholder='Número' onChange={(e) => setNumero(e.target.value)} value={numero}></input>
-                    <input required className='contato-talk__email' type='text' placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} value={email}></input>
-                    <input required className='contato-talk__mensagem' type='text' placeholder='Mensagem' onChange={(e) => setMensagem(e.target.value)} value={mensagem}></input>
-                    <input required value='Enviar' type='submit' className='contato-talk__botao'></input>
-                </form>
-            </div>
+            
         </div>
     )
 
