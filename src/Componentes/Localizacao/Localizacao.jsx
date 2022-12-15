806947238209cf38ebb7aaf72914e01a43ee3466
evaaldo <evaaldormj@gmail.com>
2022-12-15 17:23:25 -0300

    feat: adicionando mapa

--- src/Componentes/Localizacao/Localizacao.jsx
+++ src/Componentes/Localizacao/Localizacao.jsx
@@ -1,14 +1,17 @@
-import Mapa from '../Mapa/Mapa'
 import './Localizacao.css'
+import Mapa from './localizacao.png'
 
 const Localizacao = () => {
+
+    const link = 'https://www.google.com/maps/dir//Green+Barbearia+-+Rua+02,+15+A+-+Barroso,+Fortaleza+-+CE,+60863-435/@-3.8254042,-38.5160374,18.25z/data=!4m8!4m7!1m0!1m5!1m1!1s0x7c7479d915ec60b:0x989a84f0a8eba87d!2m2!1d-38.5152279!2d-3.8253756'
 
     return (
         <div className='localizacao'>
             <div className='localizacao-desktop' id='localizacao'>
                 <h1 className='localizacao-desktop__titulo'>Aonde atendemos?</h1>
                 <h1 className='localizacao-desktop__subtitulo'>Estamos em Fortaleza-CE</h1>
-                <div className='mapa'><p>mapa</p></div>
+                <a href={link} className='localizacao-desktop__descricao'>Green Barbearia, Rua 02, 15 A - Barroso, Fortaleza - CE, 60863-435</a>
+                <img src={Mapa} alt='Localização da barbearia' className='mapa'></img>
             </div>
         </div>
     )
--- src/Paginas/PaginaInicial/PaginaInicial.jsx
+++ src/Paginas/PaginaInicial/PaginaInicial.jsx
@@ -1,6 +1,7 @@
 import Cabecalho from '../../Componentes/Cabecalho/Cabecalho'
 import Contato from '../../Componentes/Contato/Contato'
 import Home from '../../Componentes/Home/Home'
+import Localizacao from '../../Componentes/Localizacao/Localizacao'
 import Preco from '../../Componentes/Preco/Preco'
 import Rodape from '../../Componentes/Rodape/Rodape'
 import './PaginaInicial.css'
@@ -12,6 +13,7 @@ const PaginaInicial = () => {
             <Cabecalho/>
             <Home/>
             <Preco/>
+            <Localizacao/>
             <Contato/>
             <Rodape/>
         </div>
